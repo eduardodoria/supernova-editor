8ac0824e8d8393082800fdf70d6e7c1d80e60f62
eduardodoria <doriaeduardo@gmail.com>
2024-09-06 14:35:41 -0300

    Supernova Editor dev start

---
+++ editor/EditorFrame.h
@@ -0,0 +1,33 @@
+#ifndef EDITORFRAME_H
+#define EDITORFRAME_H
+
+#include <wx/wx.h>
+#include <wx/splitter.h>
+#include <wx/treebase.h>
+#include <wx/treectrl.h>
+#include <wx/propgrid/propgrid.h>
+#include <wx/propgrid/advprops.h>
+
+namespace Supernova{
+    enum{
+        ID_Hello = 1
+    };
+
+    class EditorFrame : public wxFrame{
+    public:
+        EditorFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+    
+    private:
+        wxSplitterWindow* splitterMain;
+        wxSplitterWindow* splitter_left;
+        wxSplitterWindow* splitter_top;
+        wxTextCtrl* textConsole;
+
+        void OnShow(wxShowEvent& event);
+        void OnHello(wxCommandEvent& event);
+        void OnExit(wxCommandEvent& event);
+        void OnAbout(wxCommandEvent& event);
+    };
+}
+
+#endif /* EDITORFRAME_H */
---
+++ editor/main.cpp
@@ -0,0 +1,21 @@
+#include <wx/wx.h>
+#include <wx/splitter.h>
+#include <wx/treebase.h>
+#include <wx/treectrl.h>
+
+#include "EditorFrame.h"
+
+ 
+class SupernovaEditor : public wxApp{
+public:
+    bool OnInit() override;
+};
+
+wxIMPLEMENT_APP(SupernovaEditor);
+
+bool SupernovaEditor::OnInit(){
+    Supernova::EditorFrame *frame = new Supernova::EditorFrame("Supernova Editor", wxDefaultPosition, wxSize(1280, 800));
+    frame->Show(true);
+    return true;
+}
+ 
