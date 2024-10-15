579e78503158759c66973b80692d0cee866673ab
eduardodoria <doriaeduardo@gmail.com>
2024-10-14 22:46:05 -0300

    Created command class

---
+++ editor/command/CreateEntityCommand.cpp
@@ -0,0 +1,15 @@
+#include "CreateEntityCommand.h"
+
+using namespace Supernova;
+
+Editor::CreateEntityCommand::CreateEntityCommand(Project* project, uint32_t sceneId, std::string entityName){
+
+}
+
+void Editor::CreateEntityCommand::execute(){
+
+}
+
+void Editor::CreateEntityCommand::undo(){
+
+}
